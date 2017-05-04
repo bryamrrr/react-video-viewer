0ab882f8884cec08113021de1ec67e497487c9ba
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-05-04 11:38:22 -0500

    Add click in video list item

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const API_KEY = 'AIzaSyC4rPZ0GYA7eT9j0OTytBgTcbCt8K96H6Q';
 
@@ -12,10 +13,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
     })
   }
 
@@ -23,7 +30,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          videos={ this.state.videos }
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+        />
       </div>
     );
   }
